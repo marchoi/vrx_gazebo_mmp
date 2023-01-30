3009bb3f5748cb90c87418356d38d6fe78b09583
marchoi <123058648+marchoi@users.noreply.github.com>
2023-01-30 13:51:03 +0100

    Code improvements

--- positionierung.py
+++ positionierung.py
@@ -8,6 +8,7 @@ from std_msgs.msg import Empty
 from gazebo_msgs.msg import ModelStates #Zum Empfangen der Position des Bootes
 from geometry_msgs.msg import Pose
 from tf.transformations import euler_from_quaternion #Zur Umwandlung des Kurses (Heading)
+pi=3.141592654
 
 #Trajektorienplanung: Erstellen der Hindernisse und setzen der Ziele fuer das Boot
 
@@ -31,16 +32,22 @@ class Szene (): #Erstellt Tonnenpaare fuer eine Zielliste, die von der Bahnfueru
         red_bound_0=Boje(-475,185) #Tonnen auf der roten Seite
         red_bound_1=Boje(-463,201)
         red_bound_2=Boje(-454,219)
+        red_bound_3=Boje(-483,269)
         temp_r1=Boje(-468,245) #Nicht sichtbare Tonnen um die Bahnfuehrung zu verbessern
+        temp_r2=Boje(-519,221)
         temp_g1=Boje(-450,255)
+        temp_g2=Boje(-511,220)
         green_bound_0=Boje(-491,196) #Tonnen auf der gruenen Seite
         green_bound_1=Boje(-481,212)
         green_bound_2=Boje(-469,229)
+        green_bound_3=Boje(-477,258)
         tp1=Tonnenpaar(red_bound_0,green_bound_0) #Erstellen der Tonnenpaare
         tp2=Tonnenpaar(red_bound_1,green_bound_1)
         tp3=Tonnenpaar(red_bound_2,green_bound_2)
         tp4=Tonnenpaar(temp_r1,temp_g1)
-        self.ziele = [tp1,tp2,tp3,tp4] #Tonnenpaare werden in einem Array zusammengefasst
+        tp5=Tonnenpaar(red_bound_3,green_bound_3)
+        tp6=Tonnenpaar(temp_r2,temp_g2)
+        self.ziele = [tp1,tp2,tp3,tp4,tp5,tp6] #Tonnenpaare werden in einem Array zusammengefasst
 
     def getZiele (self): #Uebergabe der Zielliste an die Bahnfuehrung
         return self.ziele
@@ -58,8 +65,7 @@ class PIDRegler (): #Klasse fuer die PID Regelung der RPM (Staerke) des Bootsant
     self.err = 0.0
     self.int = 0.0
 
-  def berechnen (self,set,act): #Berechnet den Stellwert anhand der Sollgroesse und der Istgroesse
-    error = set - act; 
+  def berechnen (self,error): #Berechnet den Stellwert anhand der Sollgroesse und der Istgroesse
     P = self.kp * error;
     self.int += error * self.dt; #Aufintegrieren des Fehlers fuer I-Anteil
     I = self.ki * self.int;
@@ -88,26 +94,30 @@ class Positionierung ():
         self.bb_pub = rospy.Publisher("/wamv/thrusters/left_thrust_cmd",Float32,queue_size=10) #Zum Senden der Stellwerte an den Motor via ROS Topic
         self.stb_pub = rospy.Publisher("/wamv/thrusters/right_thrust_cmd",Float32,queue_size=10)
 
-    def bootSteuern (self, position):
+    def bootSteuern (self):
         ziel=self.ziele[self.aktuellesZiel] #Waehlt aktuelles Ziel aus Array
         zielX=ziel.mittelpunktX
         zielY=ziel.mittelpunktY
-        abstandX=zielX-position.position.x #Abstand zum Ziel
-        abstandY=zielY-position.position.y
+        abstandX=zielX-self.posX #Abstand zum Ziel
+        abstandY=zielY-self.posY
         zielAbstand=sqrt(abstandX*abstandX+abstandY*abstandY)
         zielWinkel=atan(abstandY/abstandX) #Peilung des Zieles, Winkel geht von -pi/2 bis pi/2
         if abstandX<0 and abstandY>0: #Erweiterung der Peilung auf Winkel von -pi bis pi
-            zielWinkel+=3.14159
+            zielWinkel+=pi
         elif abstandX<0 and abstandY<0:
-            zielWinkel-=3.14159
+            zielWinkel-=pi
 
-        if (zielAbstand <=4): #Wenn der Abstand zum Ziel klein genug ist (grosszuegig gewaehlt, damit genug Zeit fuer Kursaenderung)
+        if (zielAbstand <=5): #Wenn der Abstand zum Ziel klein genug ist (grosszuegig gewaehlt, damit genug Zeit fuer Kursaenderung)
             self.aktuellesZiel+= 1 #Naechstes Ziel im Array
             print ("Etappenziel erreicht")
         else:
-            zielWinkelDiff=zielWinkel-self.heading #Differenz fuer moegliche Ausgabe
-            RPMbb=self.reglerBb.berechnen(zielWinkel*-1,self.heading*-1) #Berechnung des Stellwertes Backbord Motor
-            RPMstb=self.reglerStb.berechnen(zielWinkel,self.heading) #Berechnung des Stellwertes Steuerbord Motor
+            zielWinkelDiff=zielWinkel-self.heading #Differenz fuer moegliche Ausgabe und Korrektur
+            if zielWinkelDiff > pi: #Vermeidet das Schiff sich im Kreis dreht, wenn ziel +/- pi ueberschreitet
+                zielWinkelDiff-=2*pi
+            elif zielWinkelDiff < -pi:
+                zielWinkelDiff+=2*pi
+            RPMbb=self.reglerBb.berechnen(zielWinkelDiff*-1) #Berechnung des Stellwertes Backbord Motor
+            RPMstb=self.reglerStb.berechnen(zielWinkelDiff) #Berechnung des Stellwertes Steuerbord Motor
             self.bb_pub.publish(RPMbb) #Veroeffentlichen des Stellwertes via des ROS Topics
             self.stb_pub.publish(RPMstb)
             #Verschiedene Ausgaben nach belieben ein-/auskommentierbar
@@ -132,8 +142,8 @@ class Positionierung ():
             #print ("X: %f"%self.posX)
             #print ("Y: %f"%self.posY)
             print ("Orientierung: %f"%self.heading)
-            if self.aktuellesZiel<4: #So lange ausgefuehren, wie Ziele in dem Array sind
-                self.bootSteuern (position)
+            if self.aktuellesZiel<len(self.ziele): #So lange ausgefuehren, wie Ziele in dem Array sind
+                self.bootSteuern ()
             self.counter=0
         else:
             self.counter+=1
